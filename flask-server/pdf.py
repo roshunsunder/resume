45a49db1058a04c1dc7202c1ef686d19a952b41c
Roshun Sunder <roshun.sunder@gmail.com>
2023-09-16 17:32:33 -0400

    Pushing local changes

--- flask-server/pdf.py
+++ flask-server/pdf.py
@@ -8,6 +8,8 @@ class PDFWrapper():
     def __init__(self):
         self.indent = 72
         self.newline = 14
+        self.font_size = 10
+        self.font = 'Times'
         self.pdf = FPDF(orientation='P', unit='pt', format='Letter')
         self.pdf.add_page()
         self.pdf.set_left_margin(self.indent)
@@ -15,6 +17,13 @@ class PDFWrapper():
     
     def output(self, name: str):
         self.pdf.output(name)
+    
+    def __return_thin_wide(self):
+        chars = []
+        for i in range(ord('A'), ord('z') + 1):
+            chars.append(chr(i))
+        widths = [self.pdf.get_string_width(i) for i in chars]
+        return min(widths), max(widths)
     
     def __add_line(self):
         self.pdf.ln(h=7)
@@ -27,16 +36,6 @@ class PDFWrapper():
         # Draw a line from 0 to length, at height y
         self.pdf.line(self.indent, curr_y, self.pdf.epw + self.indent, curr_y)
         self.pdf.ln(h=2)
-    
-    def chars_left(self):
-        chars_per_line = self.pdf.epw / self.pdf.font_size
-        vertical_space_left = self.pdf.y - self.pdf.b_margin
-        lines = vertical_space_left / self.pdf.font_size
-        chars = int(lines * chars_per_line)
-        self.pdf.ln(self.newline)
-        for i in range(2348):
-            self.pdf.write(txt='c')
-        return chars
     
     def add_title(
             self,
@@ -90,6 +89,21 @@ class PDFWrapper():
             self.__bulleted_list(entry['description'])
         self.pdf.ln(h=self.newline)
     
+    def chars_left(self):
+        self.pdf.set_font(self.font)
+        curr_font_size = self.pdf.font_size
+        self.pdf.set_font_size(self.font_size)
+        lines_left = int((self.pdf.eph - self.pdf.y) / self.newline)
+        self.pdf.ln(self.newline)
+        thin_char_width, wide_char_width = self.__return_thin_wide()
+        chars_per_line_lower = (self.pdf.epw / wide_char_width) - 1
+        chars_per_line_upper = (self.pdf.epw / thin_char_width) - 1
+        self.pdf.set_font_size(curr_font_size)
+        # For now, using a heuristic
+        return int((lines_left - 3) * (0.8 * chars_per_line_upper))
+        
+
+    
     def add_education(
             self,
             institution: str, 
@@ -112,17 +126,20 @@ class PDFWrapper():
         first_line = f"{degree}, {concentration}. {GPA}" if len(degree) else f"{concentration}. {GPA}"
         self.pdf.cell(w=self.pdf.epw / 2, txt= first_line, align='L')
         self.pdf.cell(w=self.pdf.epw / 2, txt = graduation, align='R')
+        self.pdf.ln(self.newline)
+    
+    def add_skills(
+            self,
+            string
+    ):
+        self.pdf.set_font(self.font, 'B',size=10)
+        self.pdf.cell(w=0, h=0, align='L', txt='SKILLS')
+        self.__add_line()
+        self.pdf.cell(txt=string)
 
 
         self.pdf.ln(self.newline + 10)
 
 string = """
-\n[\n  {\n    "company": "Cedar Inc.",\n    "title": "Software Engineering Intern",\n    "start": "May 2022",\n    "end": "August 2022",\n    "location": "New York, NY",\n    "description": [\n      "Designed and developed software solutions in Python and Django for a FinTech unicorn in an Agile environment, demonstrating solid computer science fundamentals.",\n      "Developed a notification system to streamline patient communication, showcasing excellent problem-solving skills.",\n      "Deployed code to a production environment, managing multiple tasks in a dynamic environment and reducing unnecessary call volume by over 95%."\n    ]\n  },\n  {\n    "company": "Neck App",\n    "title": "Software Engineering Intern",\n    "start": "May 2021",\n    "end": "September 2021",\n    "location": "New York, NY",\n    "description": [\n      "Designed and developed a referral program for a FinTech application using React and Typescript, demonstrating proficiency in multiple programming languages.",\n      "Managed a DocumentDB database deployment on Amazon EC2, showcasing a proven track record in software development.",\n      "Gained experience with AWS and Linux instances, furthering technical skills.",\n      "Worked on rapid sprint timelines, demonstrating strong communication and teamwork skills while ensuring data security."\n    ]\n  }\n]\n
+{\n  "Experience": [\n    {\n      "company": "Cedar Inc.",\n      "title": "Software Engineering Intern",\n      "start": "May 2022",\n      "end": "August 2022",\n      "location": "New York, NY",\n      "description": [\n        "Exhibited proficiency in Python and Django while developing for a FinTech unicorn in an Agile environment, focusing on distributed systems",\n        "Designed and implemented a notification system to streamline patient communication, aiding in bill payment",\n        "Successfully deployed code to a production environment, effectively reducing unnecessary call volume for hospitals by over 95%"\n      ]\n    },\n    {\n      "company": "Neck App",\n      "title": "Software Engineering Intern",\n      "start": "May 2021",\n      "end": "September 2021",\n      "location": "New York, NY",\n      "description": [\n        "Developed a referral program for a FinTech application, enhancing network shareability by 100% using React, React Native, and Typescript",\n        "Managed a DocumentDB database deployment on Amazon EC2, migrating existing clusters from MongoDB",\n        "Acquired expertise in AWS, database architecture, and Linux instances",\n        "Demonstrated strong teamwork skills and adaptability in rapid sprint timelines, collaborating with designers for UI implementation and ensuring backend data security"\n      ]\n    }\n  ],\n  "Projects": [\n    {\n      "company": "Sparkmate Inc.",\n      "title": "Founder, CTO",\n      "start": "May 2022",\n      "end": "Jan 2023",\n      "location": "New York, NY",\n      "description": [\n        "Led the design, architecture, and development of a unique dating app as the sole technology owner, demonstrating a proven track record in software development",\n        "Selected technology and architecture for the app to operate at scale, showcasing solid computer science fundamentals",\n        "Collaborated with the product owner to develop a profile-matching algorithm and a queuing algorithm for live video chat, powered by WebRTC"\n      ]\n    },\n    {\n      "company": "Data Science Society",\n      "title": "ML Project Creator / Leader",\n      "start": "September 2020",\n      "end": "May 2021",\n      "location": "Berkeley, CA",\n      "description": [\n        "Utilized Python and related libraries to develop an algorithm for political article analysis, demonstrating excellent problem-solving skills",\n        "Processed a dataset of 15,000+ articles using VADER sentiment analysis and other data science/ML techniques, achieving over 80% accuracy",\n        "Led a team in the creation of the algorithm and its accompanying data visualizations, demonstrating strong communication and teamwork skills"\n      ]\n    }\n  ]\n}
 """
-
-pdf = PDFWrapper()
-pdf.add_title('Roshun Sunder', '508 W 114th St, New York, NY 10025', 'roshun.sunder@gmail.com', '503-686-3249', 'roshunsunder.com')
-pdf.add_education('Columbia University', 'New York, NY', 'May 2024', 'Computer Science', degree='BA', GPA='3.8')
-pdf.add_generic_section('EXPERIENCE', json.loads(string))
-pdf.chars_left()
-pdf.output('automated.pdf')
--- flask-server/utils.py
+++ flask-server/utils.py
@@ -5,6 +5,9 @@ import os
 import json
 from pdf import PDFWrapper
 
+# Testing with only one pdf
+_pdf = PDFWrapper()
+
 def get_completion(prompt, model="gpt-4", temp=0):
     load_dotenv()
     api_key = os.getenv('OPENAI_API_KEY')
@@ -17,13 +20,18 @@ def get_completion(prompt, model="gpt-4", temp=0):
     )
     return response.choices[0].message["content"]
 
+def add_header():
+    _pdf.add_title('Roshun Sunder', '508 W 114th St, New York, NY 10025', 'roshun.sunder@gmail.com', '503-686-3249', 'roshunsunder.com')
+    _pdf.add_education('Columbia University', 'New York, NY', 'May 2024', 'Computer Science', degree='BA', GPA='3.8')
+
 def naked_query(job_desc: str, input_json: str):
+    add_header()
     prompt = f"""
     You are a resume building expert.  I am going to first give you a job description.  \
     You will understand the roles and responsibilities of the job.  You will also understand the qualifications of the job.  \
-    Then I will give you work experiences of a candidate in JSON format.
+    Then I will give you work the work history of a candidate in JSON format.
 
-    Your job is to rewrite the job experience to match the job \
+    Your job is to rewrite their resume to match the job \
     description, highlighting experiences relevant to the job.  First I will start with the job description, \
     which is delimited by triple backticks.
 
@@ -31,12 +39,15 @@ def naked_query(job_desc: str, input_json: str):
 
     Now, rewrite and return the following roles and responsibilities of the candidate to better \
     match the keywords of the job description. If the candidate has included numbers quanitfying \
-    impact, try to include them in your final result.
+    impact, try to include them in your final result. Try not to repeat keywords and avoid using \
+    repetitive language patterns.
 
-    Return only well-formatted JSON. The character count for all the experiences should not total more than 1450 characters.
+    Return only well-formatted JSON. The character count should not total more than 1450 characters.
     CANDIDATE EXPERIENCE JSON: {input_json}"""
 
     res = get_completion(prompt)
+
+    print(f"Res: {res}")
 
     cleaning_prompt = f"""
     You are a quality analyst for an program that automatically edits resumes.
@@ -48,16 +59,20 @@ def naked_query(job_desc: str, input_json: str):
     It is possible that the program worked perfectly, and no extra technical skills were \
     mistakenly included. In that case, simply return the EDITED resume as is.
 
-    Return only the JSON object after you have corrected it.
+    Return only the final JSON object. Do not write anything else.
 
     ORIGINAL: {input_json}
     EDITED: {res}
     """
 
     cleaned = get_completion(cleaning_prompt)
-    pdf = PDFWrapper()
-    pdf.add_title('Roshun Sunder', '508 W 114th St, New York, NY 10025', 'roshun.sunder@gmail.com', '503-686-3249', 'roshunsunder.com')
-    pdf.add_education('Columbia University', 'New York, NY', 'May 2024', 'Computer Science', degree='BA', GPA='3.8')
-    pdf.add_generic_section('EXPERIENCE', json.loads(cleaned))
-    pdf.output('automated.pdf')
+
+    print(f"Cleaned: {cleaned}")
+    for section_name, information in json.loads(cleaned).items():
+        _pdf.add_generic_section(section_name.upper(), information)
+    _pdf.add_skills('Python | Java | C | SQL | API Development | Mobile Development | Web Applications | MongoDB | Django')
+    _pdf.output('automated.pdf')
     return './automated.pdf'
+
+
+
